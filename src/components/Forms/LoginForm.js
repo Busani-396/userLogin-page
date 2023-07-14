ffa743c78c7d2f66f622405386ee19cd6f110ca7
saniby <mbusani66@gmail.com>
2023-07-14 22:29:22 +0200

    added effects on login form btn

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import '../Forms/form.css'
 
 const LoginForm = ()=>{
@@ -25,13 +25,16 @@ const LoginForm = ()=>{
                   } 
                  }
         )
-                }
-
-    // if(user.username && user.password){
-    //     setActive(true);
-    // }else{
-    //     setActive(false)
-    // }
+ 
+    }
+useEffect(()=>{
+    if(user.username && user.password){
+        setActive(true);
+    }else{
+        setActive(false)
+    }
+})
+    
 
     const lockIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-lock" viewBox="0 0 16 16">
                          <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
